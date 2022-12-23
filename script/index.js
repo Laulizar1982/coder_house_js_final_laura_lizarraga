b52e6159484b4d382b853df27b19cc2fb64dccea
Laulizar1982 <laulizar@gmail.com>
2022-12-22 22:57:23 -0300

    clima funcionando

--- script/index.js
+++ script/index.js
@@ -304,3 +304,30 @@ function actualizarNumero() {
     let nuevoNumero = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
     numero.innerText = nuevoNumero;
 }
+
+// INFORMACION DE LA API
+
+let clima = {
+  apiKey: "5e74f0be2e35f69fa9e9cab8882e8d29",
+  fetchClima: function(ciudad){
+  fetch ("https://api.openweathermap.org/data/2.5/weather?q="
+  +ciudad
+  + "&units=metric&appid="
+  + this.apiKey
+  )
+  .then ((response) => {
+    return response.json ()
+  })
+  .then ((data)=>this.mostrarClima(data));
+    
+},
+mostrarClima:function(data){
+  const {temp} = data.main;
+  document.querySelector ("#temp").innerHTML = temp + " &#8451;"
+}
+}
+let ciudad = document.querySelector ("#ciudad");
+ciudad.addEventListener ("change" , function (){
+  clima.fetchClima (ciudad.value)
+})
+clima.fetchClima ("buenos aires")
